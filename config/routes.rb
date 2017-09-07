6d301e72e96ca0321e530f097e5fcedb9ba641b2
neeltrivedi <neel.trivedi@technitask.com>
2017-09-06 21:17:12 -0400

    adding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace 'api' do
+    namespace 'v1' do
+      resources :patients do
+        resources :encounters
+      end
+    end
+  end
 end
