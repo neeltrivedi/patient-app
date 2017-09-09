24be46afee9ab6e7e5d331ee67434646a13130d1
neeltrivedi <neel.trivedi@technitask.com>
2017-09-09 16:33:09 -0400

    Creating Header component

---
+++ app/javascript/patients/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render () {
+    return (
+      <div>
+        <Navbar color="faded" inverse toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">PatientApp</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="https://github.com/neeltrivedi/patient-app">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
