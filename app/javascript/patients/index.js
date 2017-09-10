f7499f318902edb7e464dc36fcaa4e3dee770da0
neeltrivedi <neel.trivedi@technitask.com>
2017-09-10 13:19:02 -0400

    configuring to use redux store

--- app/javascript/patients/index.js
+++ app/javascript/patients/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 import App from './components/App'
 
 
 const patients = document.querySelector('#patients')
-ReactDOM.render(<App />, patients)  
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  patients
+)  
