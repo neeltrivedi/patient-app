16b89b7e463ba3541fdfd5360e543d8114d40369
Neel Trivedi <neel.trivedi@thinkresearch.com>
2017-10-11 13:06:45 -0400

    directly using params

--- app/interactors/create_patient.rb
+++ app/interactors/create_patient.rb
@@ -2,9 +2,8 @@ class CreatePatient
   include Interactor
 
   def call
-    hash_for_interactor = {mrn: context[:mrn], first_name: context[:first_name], middle_name: context[:middle_name], last_name: context[:last_name], weight: context[:weight], height: context[:height]}
-    patient = Patient.new(hash_for_interactor)
-    
+    patient = Patient.new(context.patient)
+
     if patient.save
       context.patient = patient
     else
