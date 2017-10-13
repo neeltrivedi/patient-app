232b6d69f723883910a4c1b3b0c3376330147ce6
Neel Trivedi <neel.trivedi@thinkresearch.com>
2017-10-13 13:17:46 -0400

    Updating encounters controller to use interactors

--- app/controllers/api/v1/encounters_controller.rb
+++ app/controllers/api/v1/encounters_controller.rb
@@ -2,34 +2,37 @@ module Api
   module V1
     class EncountersController < ApplicationController
       def index
-      	patient = Patient.find(params[:patient_id])
-      	encounters = patient.encounters
-        render json: {status: 'SUCCESS', message:'Loaded Patient Encounters', data: encounters},status: :ok
+        result = FindPatient.call({id: params[:patient_id]})
+        result_encounters = AllEncountersPatient.call({patient: result.patient})
+        render json: {status: 'SUCCESS', message:'Loaded Patient Encounters', data: result_encounters.encounters},status: :ok
       end
 
       def show
-        patient = Patient.find(params[:patient_id])
-  	    encounter = patient.encounters.find(params[:id])
-        render json: {status: 'SUCCESS', message:'Loaded Patient encounter', data: encounter},status: :ok
+        result = FindPatient.call({id: params[:patient_id]})
+        result_encounter = FindEncounter.call({id: params[:id], patient: result.patient})
+        render json: {status: 'SUCCESS', message:'Loaded Patient encounter', data: result_encounter.encounter},status: :ok
       end
 
       def create
-        patient = Patient.find(params[:patient_id])
-        encounter = patient.encounters.create(encounter_params)
+        result = FindPatient.call({id: params[:patient_id]})
+        result_encounter = CreateEncounter.call({patient: result.patient, encounter: encounter_params})
 
-        if encounter.save
-          render json: {status: 'SUCCESS', message:'Saved Patient encounter', data: encounter},status: :ok
+        if result_encounter.success?
+          render json: {status: 'SUCCESS', message:'Saved Patient encounter', data: result_encounter.encounter},status: :ok
         else
-          render json: {status: 'ERROR', message:'Not able to save Patient encounter', data: encounter.errors},status: :unprocessable_entity
+          render json: {status: 'ERROR', message:'Not able to save Patient encounter', data: result_encounter.encounter.errors},status: :unprocessable_entity
         end
 
       end
 
       def destroy
-        patient = Patient.find(params[:patient_id])
-      	encounter = patient.encounters.find(params[:id])
-      	encounter.destroy
-        render json: {status: 'SUCCESS', message:'Deleted Patient encounter', data: encounter},status: :ok
+        result = FindPatient.call({id: params[:patient_id]})
+        result_encounter = FindEncounter.call({id: params[:id], patient: result.patient})
+        delete_encounter = DeleteRecord.call(record: result_encounter.encounter)
+
+        if delete_encounter.success?
+        render json: {status: 'SUCCESS', message:'Deleted Patient encounter', data: delete_encounter.encounter},status: :ok
+        end
       end
 
       def update
