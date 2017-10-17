541db55d582dcac177de7f600753a4e141fa5efa
Neel Trivedi <neel.trivedi@thinkresearch.com>
2017-10-17 12:08:46 -0400

    Updating controller test

--- test/controllers/api/v1/encounters_controller_test.rb
+++ test/controllers/api/v1/encounters_controller_test.rb
@@ -1,12 +1,34 @@
 require "test_helper"
 module Api
   module V1
-    describe EncountersController do
-      # it "#index" do
-      #   # get '/api/v1/encounters#index'
-      #   get '/api/v1/patients/3/encounters'
-      #   assert_response :ok
-      # end
+    class EncountersControllerTest < ActionController::TestCase
+      def setup
+        super
+        @patient = patients(:one)
+        @encounter = encounters(:one)
+        # @patient = FactoryGirl.create :patient
+      end
+
+      test "#index" do
+        get :index, params: { patient_id: @patient }
+        assert_response :ok
+      end
+
+      test "#show" do
+        get :show, params: { patient_id: @patient, id: @encounter }
+        assert_response :ok
+      end
+
+      test "#create" do
+        post :create, params: { patient_id: @patient, encounter: @encounter.attributes }
+        assert_response :ok
+      end
+
+      test "#destroy" do
+        delete :destroy, params: { patient_id: @patient, id: @encounter }
+        assert_response :ok
+      end
+
     end
   end
 end
--- test/controllers/api/v1/patients_controller_test.rb
+++ test/controllers/api/v1/patients_controller_test.rb
@@ -1,25 +1,67 @@
 require "test_helper"
+require "factory_girl_rails"
+
 module Api
   module V1
-    describe PatientsController do
-      setup do
-        @patient = patients(:one)
-      end
+    class PatientsControllerTest < ActionController::TestCase
+        def setup
+          super
+          @patient = patients(:one)
+          # @patient = FactoryGirl.create :patient
+          # p '============='
+          # p @patient
+        end
 
-      it "#index" do
-        get api_v1_patients_path
-        assert_response :ok
-      end
+        test '#index' do
+          get :index
+          assert_response :ok
+        end
 
-      it "#show" do
-        get api_v1_patient_path(@patient)
-        assert_response :ok
-      end
+        # test "#show" do
+        #   get :show, params: { id: @patient }
+        #   assert_response :ok
+        # end
+        #
+        # test "#create" do
+        #   post :create, params: { patient: @patient.attributes }
+        #   assert_response :ok
+        # end
+        #
+        # test "#destroy" do
+        #   delete :destroy, params: { id: @patient }
+        #   assert_response :ok
+        # end
 
-      it "#create" do
-        post api_v1_patients_path, params: { patient: @patient.attributes }
-        assert_response :ok
-      end
+        # test "#edit" do
+        #   put :update, params: { id: @patient, patient_params: @patient.attributes }
+        #   assert_response :ok
+        # end
     end
   end
 end
+
+
+# module Api
+#   module V1
+#     describe PatientsController do
+#       setup do
+#         @patient = patients(:one)
+#       end
+#
+#       it "#index" do
+#         get api_v1_patients_path
+#         assert_response :ok
+#       end
+#
+#       it "#show" do
+#         get api_v1_patient_path(@patient)
+#         assert_response :ok
+#       end
+#
+#       it "#create" do
+#         post api_v1_patients_path, params: { patient: @patient.attributes }
+#         assert_response :ok
+#       end
+#     end
+#   end
+# end
